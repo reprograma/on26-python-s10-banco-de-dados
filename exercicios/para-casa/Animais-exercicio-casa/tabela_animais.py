27e25f1364ebd432ed9a23e87ce626b2b237b85d
Mariana-Ol <mariiolivi@gmail.com>
2023-10-13 15:54:14 -0300

    Atualizei o arquivo tabela_animais.py

--- exercicios/para-casa/Animais-exercicio-casa/tabela_animais.py
+++ exercicios/para-casa/Animais-exercicio-casa/tabela_animais.py
@@ -4,31 +4,35 @@ import csv
 banco = sqlite3.connect('Animais.db')
 manipulador_do_banco = banco.cursor()
 
-# manipulador_do_banco.execute('''CREATE TABLE animais_atualizar (Animal TEXT,
-# Altura TEXT,
-# Peso TEXT,
-# Cor TEXT,
-# Expectativa_vida_em_anos TEXT,
-# Dieta TEXT,
-# Habitat TEXT,
-# Predadores TEXT,
-# Velocidade_média TEXT,
-# Países_em_que_é_encontrado TEXT,
-# Estado_preservação TEXT,
-# Família TEXT,
-# Período_gestação_em_dias TEXT,
-# Velocidade_máxima TEXT,
-# Estrutura_social TEXT,
-# Número_filhotes_por_ninhada TEXT)''')
+manipulador_do_banco.execute('''CREATE TABLE animais2 (Animal TEXT,
+Altura TEXT,
+Peso TEXT,
+Cor TEXT,
+Expectativa_vida_em_anos TEXT,
+Dieta TEXT,
+Habitat TEXT,
+Predadores TEXT,
+Velocidade_média TEXT,
+Países_em_que_é_encontrado TEXT,
+Estado_preservação TEXT,
+Família TEXT,
+Período_gestação_em_dias TEXT,
+Velocidade_máxima TEXT,
+Estrutura_social TEXT,
+Número_filhotes_por_ninhada TEXT)''')
 
 file = open('Animal Dataset.csv')
 next(file)
 conteudo = csv.reader(file)
 
-"""Inseri uma última linha na tabela com o animal Boi"""
-inserir_conteudo = "INSERT INTO animais2 (Animal, Altura, Peso, Cor, Expectativa_vida_em_anos, Dieta, Habitat, Predadores, Velocidade_média, Países_em_que_é_encontrado, Estado_preservação, Família, Período_gestação_em_dias, Velocidade_máxima, Estrutura_social, Número_filhotes_por_ninhada) VALUES ('Boi', 'baixo', 'pesado', 'marrom', 'muitos', 'grama', 'pastos', 'talvez uma jiboia', 'devagar e sempre', 'praticamente todos', 'conservado', 'disfuncional', '0', 'devagar', 'sempre com amigos', '0')"
 
-manipulador_do_banco.execute(inserir_conteudo)
+inserir_conteudo = "INSERT INTO animais2 (Animal, Altura, Peso, Cor, Expectativa_vida_em_anos, Dieta, Habitat, Predadores, Velocidade_média, Países_em_que_é_encontrado, Estado_preservação, Família, Período_gestação_em_dias, Velocidade_máxima, Estrutura_social, Número_filhotes_por_ninhada) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+manipulador_do_banco.executemany(inserir_conteudo, conteudo)
+
+"""Inseri uma nova linha no fim da tabela"""
+inserir_linha = "INSERT INTO animais2 (Animal, Altura, Peso, Cor, Expectativa_vida_em_anos, Dieta, Habitat, Predadores, Velocidade_média, Países_em_que_é_encontrado, Estado_preservação, Família, Período_gestação_em_dias, Velocidade_máxima, Estrutura_social, Número_filhotes_por_ninhada) VALUES ('a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a')"
+manipulador_do_banco.execute(inserir_linha)
 
 selecionar_tudo = "SELECT * FROM  animais2"
 entradas = manipulador_do_banco.execute(selecionar_tudo).fetchall()
@@ -36,12 +40,12 @@ entradas = manipulador_do_banco.execute(selecionar_tudo).fetchall()
 banco.commit();
 banco.close();
 
-"""Excluí as linhas correspondentes aos animais Aardvark e Anteater"""
+"""Excluí as linhas correspondentes aos animais Aardvark"""
 
 def excluir(Animal):
     banco = sqlite3.connect('Animais.db')
     manipulador_do_banco = banco.cursor() 
-    excluir_conteudo = "DELETE FROM animais_excluir_linha WHERE Animal = ?"
+    excluir_conteudo = "DELETE FROM animais2 WHERE Animal = ?"
     manipulador_do_banco.execute(excluir_conteudo, (Animal, ))
     banco.commit()
     banco.close()
@@ -52,7 +56,7 @@ def excluir(Animal):
 def atualizar(Animal, novo_valor, campo):
     banco = sqlite3.connect('Animais.db')
     manipulador_do_banco = banco.cursor()
-    atualizar_conteudo = f"UPDATE animais_atualizar SET {campo} = ? WHERE Animal = ? "
+    atualizar_conteudo = f"UPDATE animais2 SET {campo} = ? WHERE Animal = ? "
     manipulador_do_banco.execute(atualizar_conteudo, (novo_valor, Animal))
     banco.commit()
     banco.close()
